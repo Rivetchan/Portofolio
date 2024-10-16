77e6303fb5007806b0f44f2a2266251eb6d4ead8
Rivet <139978419+Rivetchan@users.noreply.github.com>
2024-10-16 16:53:55 +0700

    Tambahan Event dan Album

---
+++ Latihan/latihan.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const totalImages = document.querySelectorAll('.latihan-gallery img').length;
+    document.getElementById('total-foto').textContent = 'Total latihan: ' + totalImages;
+});
+
+document.querySelectorAll('.latihan-gallery img').forEach((image) => {
+    image.addEventListener('mouseover', function() {
+        image.style.transform = "scale(1.2)";  
+        image.style.transition = "transform 0.5s ease";  
+        image.style.boxShadow = "0 8px 30px rgba(255, 0, 153, 0.8), 0 8px 50px rgba(255, 0, 153, 0.6)";
+    });
+
+    image.addEventListener('mouseout', function() {
+        image.style.transform = "scale(1)";
+        image.style.transition = "transform 0.5s ease";  
+        image.style.boxShadow = "0 4px 15px rgba(255, 0, 153, 0.6)";
+    });
+});
